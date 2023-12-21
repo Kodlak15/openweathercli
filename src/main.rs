b4e50fd2c05ddeac431b713e778c404a34f69858
Kodlak15 <stanlcod15@gmail.com>
2023-12-20 22:08:21 -0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -26,66 +26,96 @@ impl Environment {
     }
 }
 
+#[derive(serde::Deserialize)]
 struct Coord {
-    lon: Option<String>,
-    lat: Option<String>,
+    lon: Option<f32>,
+    lat: Option<f32>,
 }
 
+#[derive(serde::Deserialize)]
 struct Weather {
-    id: Option<String>,
+    id: Option<i32>,
     main: Option<String>,
     description: Option<String>,
     icon: Option<String>,
 }
 
+#[derive(serde::Deserialize)]
 struct Main {
-    temp: Option<String>,
-    feels_like: Option<String>,
-    temp_min: Option<String>,
-    temp_max: Option<String>,
-    pressure: Option<String>,
-    humidity: Option<String>,
-    sea_level: Option<String>,
-    grnd_level: Option<String>,
+    temp: Option<f32>,
+    feels_like: Option<f32>,
+    temp_min: Option<f32>,
+    temp_max: Option<f32>,
+    pressure: Option<i32>,
+    humidity: Option<i32>,
+    sea_level: Option<i32>,
+    grnd_level: Option<i32>,
 }
 
+#[derive(serde::Deserialize)]
 struct Wind {
-    speed: Option<String>,
-    deg: Option<String>,
-    gust: Option<String>,
+    speed: Option<f32>,
+    deg: Option<i32>,
+    gust: Option<f32>,
 }
 
+#[derive(serde::Deserialize)]
 struct Rain {
-    _1h: Option<String>,
+    _1h: Option<f32>,
 }
 
+#[derive(serde::Deserialize)]
 struct Clouds {
-    all: Option<String>,
+    all: Option<i32>,
 }
 
+#[derive(serde::Deserialize)]
 struct Sys {
-    r#type: Option<String>,
-    id: Option<String>,
+    r#type: Option<i32>,
+    id: Option<i32>,
     country: Option<String>,
-    sunrise: Option<String>,
-    sunset: Option<String>,
+    sunrise: Option<i32>,
+    sunset: Option<i32>,
 }
 
+#[derive(serde::Deserialize)]
 struct CurrentWeather {
     coord: Option<Coord>,
     weather: Option<Weather>,
     base: Option<String>,
     main: Option<Main>,
-    visibility: Option<String>,
+    visibility: Option<i32>,
     wind: Option<Wind>,
     rain: Option<Rain>,
     clouds: Option<Clouds>,
-    dt: Option<String>,
+    dt: Option<i32>,
     sys: Option<Sys>,
-    timezone: Option<String>,
-    id: Option<String>,
+    timezone: Option<i32>,
+    id: Option<i32>,
     name: Option<String>,
-    cod: Option<String>,
+    cod: Option<i32>,
+}
+
+impl CurrentWeather {
+    async fn get(key: String) -> Result<Self, reqwest::Error> {
+        let req_uri = format!(
+            "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}",
+            0, 0, key
+        );
+
+        let response = reqwest::get(req_uri).await?;
+
+        if !response.status().is_success() {
+            println!("Response failed with status code {}", response.status());
+            panic!("Failed to get response, aborting!")
+        }
+
+        let body = response.text().await?;
+        let weather: CurrentWeather =
+            serde_json::from_str(&body).expect("Failed to deserialize response body!");
+
+        Ok(weather)
+    }
 }
 
 // this is only for current weather!
@@ -103,16 +133,22 @@ async fn get_response(environment: &Environment) -> Response {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), reqwest::Error> {
     let environment = Environment::load();
 
-    let req_uri = format!(
-        "https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&exclude={}&appid={}",
-        0, 0, "", environment.key
-    );
+    let weather = CurrentWeather::get(environment.key).await?;
 
-    let res = get_response(&environment).await;
+    println!("Lat: {}", weather.coord.unwrap().lat.unwrap());
 
-    println!("Key: {}", environment.key);
-    println!("Response: {}", res.text().await.expect("???"));
+    Ok(())
+
+    // let req_uri = format!(
+    //     "https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&exclude={}&appid={}",
+    //     0, 0, "", environment.key
+    // );
+    //
+    // let res = get_response(&environment).await;
+    //
+    // println!("Key: {}", environment.key);
+    // println!("Response: {}", res.text().await.expect("???"));
 }
