b74fb5c1699972c6edaa2055c570c3c12e57d26a
Kodlak15 <stanlcod15@gmail.com>
2024-01-05 16:41:26 -0800

    Work on five day forecast, allow api selection

--- src/data/five_day_forecast.rs
+++ src/data/five_day_forecast.rs
@@ -1,8 +1,11 @@
-use itertools::Itertools;
+use itertools::{GroupBy, Itertools};
 use serde::Deserialize;
 
 use crate::{
-    data::data::Data,
+    data::{
+        convert::{to_celsius, to_fahrenheight},
+        data::Data,
+    },
     options::{
         args::Args,
         environment::Environment,
@@ -14,21 +17,9 @@ use crate::{
 
 use super::geocoding::Geocoding;
 
-fn get_high_temps(data: FiveDayForecast) -> Vec<f32> {
-    // data.list
-    //     .expect("Could not unpack list!")
-    //     .iter()
-    //     .map(|day| {
-    //
-    //     });
+fn get_days() {}
 
-    vec![]
-}
-
-fn get_low_temps(data: FiveDayForecast) -> Vec<f32> {
-    vec![]
-}
-
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct Main {
     temp: Option<f32>,
@@ -42,6 +33,7 @@ pub struct Main {
     temp_kf: Option<f32>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct Weather {
     id: Option<i32>,
@@ -50,11 +42,13 @@ pub struct Weather {
     icon: Option<String>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct Clouds {
     all: Option<i8>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct Wind {
     speed: Option<f32>,
@@ -62,23 +56,27 @@ pub struct Wind {
     gust: Option<f32>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct Rain {
     _1h: Option<f32>,
     _3h: Option<f32>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct Snow {
     _1h: Option<f32>,
     _3h: Option<f32>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct Sys {
     pod: Option<String>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct Day {
     dt: Option<i32>,
@@ -94,6 +92,7 @@ pub struct Day {
     dt_txt: Option<String>,
 }
 
+#[allow(dead_code)]
 #[derive(Deserialize, Clone, Debug)]
 pub struct FiveDayForecast {
     cod: Option<String>,
@@ -179,56 +178,101 @@ impl FiveDayForecast {
                 true => {
                     println!("Five day forecast:");
 
-                    let tmax: Vec<f32> = days
+                    // TODO: consider pulling this procedure into its own function
+                    // The temperatures being presented here seem wrong as well
+                    let (date, (tmax, tmin)): (Vec<String>, (Vec<f32>, Vec<f32>)) = days
                         .into_iter()
                         .map(|(_, day)| {
-                            let day = &day.collect::<Vec<&Day>>();
+                            let day = day.collect::<Vec<&Day>>();
                             day.iter()
                                 .map(|day| {
-                                    day.main
+                                    let date = day
+                                        .dt_txt
                                         .clone()
-                                        .expect("Could not unpack main!")
-                                        .temp_max
-                                        .expect("Could not unpack max temperature!")
+                                        .expect("Could not unpack datetime string!")
+                                        .split_whitespace()
+                                        .next()
+                                        .expect("Could not unpack stripped datetime string!")
+                                        .to_owned();
+
+                                    let main = day.main.clone().expect("Could not unpack main!");
+
+                                    let tmax =
+                                        main.temp_max.expect("Could not unpack max temperature!");
+
+                                    let tmin =
+                                        main.temp_min.expect("Could not unpack min temperature!");
+
+                                    (date, tmax, tmin)
                                 })
-                                .fold(None, |acc, x| {
+                                .fold(None, |acc, (date, x, y)| {
                                     Some(match acc {
-                                        Some(max) => f32::max(max, x),
-                                        None => x,
+                                        Some((date, max, min)) => {
+                                            (date, f32::max(max, x), f32::min(min, y))
+                                        }
+                                        None => (date, x, y),
                                     })
                                 })
                                 .expect("Could not unpack max temperature for day!")
                         })
-                        .collect();
+                        .map(|(date, tmax, tmin)| match units {
+                            "M" => (date, (to_celsius(tmax), to_celsius(tmin))),
+                            "I" => (date, (to_fahrenheight(tmax), to_fahrenheight(tmin))),
+                            _ => (date, (tmax, tmin)),
+                        })
+                        .unzip();
 
-                    let tmin: Vec<f32> = days
+                    for (date, (max, min)) in date.iter().zip(tmax.iter().zip(tmin.iter())) {
+                        println!("[{}] High: {:.2}, Low: {:.2}", date, max, min);
+                    }
+                }
+                false => {
+                    let (date, (tmax, tmin)): (Vec<String>, (Vec<f32>, Vec<f32>)) = days
                         .into_iter()
                         .map(|(_, day)| {
-                            let day = &day.collect::<Vec<&Day>>().clone();
+                            let day = day.collect::<Vec<&Day>>();
                             day.iter()
                                 .map(|day| {
-                                    day.main
+                                    let date = day
+                                        .dt_txt
                                         .clone()
-                                        .expect("Could not unpack main!")
-                                        .temp_min
-                                        .expect("Could not unpack min temperature!")
+                                        .expect("Could not unpack datetime string!")
+                                        .split_whitespace()
+                                        .next()
+                                        .expect("Could not unpack stripped datetime string!")
+                                        .to_owned();
+
+                                    let main = day.main.clone().expect("Could not unpack main!");
+
+                                    let tmax =
+                                        main.temp_max.expect("Could not unpack max temperature!");
+
+                                    let tmin =
+                                        main.temp_min.expect("Could not unpack min temperature!");
+
+                                    (date, tmax, tmin)
                                 })
-                                .fold(None, |acc, x| {
+                                .fold(None, |acc, (date, x, y)| {
                                     Some(match acc {
-                                        Some(min) => f32::min(min, x),
-                                        None => x,
+                                        Some((date, max, min)) => {
+                                            (date, f32::max(max, x), f32::min(min, y))
+                                        }
+                                        None => (date, x, y),
                                     })
                                 })
-                                .expect("Could not unpack min temperature for day!")
+                                .expect("Could not unpack max temperature for day!")
                         })
-                        .collect();
+                        .map(|(date, tmax, tmin)| match units {
+                            "M" => (date, (to_celsius(tmax), to_celsius(tmin))),
+                            "I" => (date, (to_fahrenheight(tmax), to_fahrenheight(tmin))),
+                            _ => (date, (tmax, tmin)),
+                        })
+                        .unzip();
 
-                    // TODO: handle unit conversions
-                    // Also, tmax operation seems to leave no data for tmin YAYYYYYYY
-                    println!("Tmax: {:?}", tmax);
-                    println!("Tmin: {:?}", tmin);
+                    for (date, (max, min)) in date.iter().zip(tmax.iter().zip(tmin.iter())) {
+                        println!("[{}] {:.2}, {:.2}", date, max, min);
+                    }
                 }
-                false => todo!(),
             },
             _ => println!("No data to print for option {}", opt),
         };
