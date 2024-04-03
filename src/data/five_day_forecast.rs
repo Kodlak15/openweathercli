fcd6c86cc1d634d2d74009f128f134c0ee367985
Kodlak15 <stanlcod15@gmail.com>
2024-04-02 19:26:08 -0700

    Committing old changes

--- src/data/five_day_forecast.rs
+++ src/data/five_day_forecast.rs
@@ -220,6 +220,8 @@ impl FiveDayForecast {
                         })
                         .unzip();
 
+                    let (date, (tmin, tmax)) = get_daily_temps(days, units);
+
                     for (date, (max, min)) in date.iter().zip(tmax.iter().zip(tmin.iter())) {
                         println!("[{}] High: {:.2}, Low: {:.2}", date, max, min);
                     }
