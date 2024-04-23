d0b83549d340d79578a384648c2914930412d2db
Kodlak15 <stanlcod15@gmail.com>
2024-04-22 23:33:38 -0700

    Fix merge conflicts

--- src/data/data.rs
+++ src/data/data.rs
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-use super::{current_weather::CurrentWeather, five_day_forecast::FiveDayForecast};
-
-pub enum Data {
-    CurrentWeather(CurrentWeather),
-    FiveDayForecast(FiveDayForecast),
-=======
 use super::current_weather::CurrentWeather;
 
 pub enum Data {
     CurrentWeather(CurrentWeather),
->>>>>>> dev
 }
